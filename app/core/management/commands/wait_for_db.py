76bee8856e5ce5e1e778d42a837ed48d70d7c150
kysniw <kuba.winsyk.forgames@gmail.com>
2023-04-28 01:04:56 +0200

    Configured docker compose and waiting fot db

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -5,6 +5,7 @@ from psycopg2 import OperationalError as Psycopg2Error
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
@@ -15,7 +16,8 @@ class Command(BaseCommand):
                 self.check(databases=['default'])
                 db_up = True
             except (Psycopg2Error, OperationalError):
-                self.stdout.write("Database still proccessing, waiting 1 second...")
+                self.stdout.write("Database still proccessing, \
+                                   waiting 1 second...")
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS('Database running well!'))
