d04f13f9824c1cbe2a06d4ef537ca2c40825170d
kysniw <kuba.winsyk.forgames@gmail.com>
2023-05-11 17:28:12 +0200

    Continue working with docker-compose and django with recipe app tutorial

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,61 @@
+from decimal import Decimal
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+from core import models
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_success(self):
+        email = "test@example.com"
+        password = "testexample123"
+        user = get_user_model().objects.create_user(
+            email = email,
+            password = password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com']
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'sample123')
+
+    def test_create_superuser(self):
+        user = get_user_model().objects.create_superuser(
+            email='test@example.com',
+            password='sample123'
+        )
+
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
+
+    def test_create_recipe(self):
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'test123'
+        )
+
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample recipe name',
+            time_minutes=5,
+            price=Decimal(5.50),
+            description='Sample recipe description'
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
